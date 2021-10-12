dd2f0b6a3d3401558b15a0f7e5b83c31c382b51d
Alexander Demchak <demchak.alex@gmail.com>
2021-10-12 14:56:35 -0700

    update functions.js to work

--- calculators/functions.js
+++ calculators/functions.js
@@ -1,3 +1,8 @@
+/* jshint esversion: 6 */
+const head       = document.head || document.getElementsByTagName('head')[0],
+	  script     = document.currentScript ?? document.scripts[document.scripts.length - 1],
+	  calculator = document.currentScript.getAttribute('calculator');
+
 function resetForm(el,e){
 	e.preventDefault();
 	
@@ -9,4 +14,25 @@ function resetForm(el,e){
 	fields.forEach(function(field){
 		field.value = '';
 	});
+}
+
+function loadCalculator(){
+	if( document.documentElement.dataset.['loadCalculator-'+calculator] === 'true' )
+		return;
+
+	var templateURL = 'https://cdn.jsdelivr.net/gh/Third-River-Marketing-LLC/lead-connector-website-modules/calculators/'+ calculator +'/calc-template.html';
+
+	fetch(templateURL).then(function(response){
+		return response.json();
+	}).then(function(json){
+		console.log( json );
+		script.outerHTML = '';
+		document.documentElement.dataset.['loadCalculator-'+calculator] = 'true';
+	});
+}
+
+if( document.readyState === 'loading' ) {
+	document.addEventListener( 'DOMContentLoaded', loadCalculator );
+} else if( document.readyState === 'interactive' || document.readyState === 'complete' ) {
+	loadCalculator();
 }
