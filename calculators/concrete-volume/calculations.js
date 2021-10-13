079ee8464bbda23d6abd9faede072f436e8609d9
Alexander Demchak <demchak.alex@gmail.com>
2021-10-13 10:52:58 -0700

    Update calculations.js

--- calculators/concrete-volume/calculations.js
+++ calculators/concrete-volume/calculations.js
@@ -30,9 +30,19 @@ function calculateCubicYards(el, e){
 	Object.keys(fields).forEach(function(key){
 		if( key == 'answer' )
 			return;
+		
+		var step  = fields[key].getAttribute('step');
+			dec   = 1,
+			fixed = 0;
+
+		if( step != null && step.includes('.') )
+			fixed = step.replace('.','').length;
+	
+		dec += '0'.repeat(fixed);
+		dec = Number(dec);
 
 		// Force to fixed step
-		fields[key].value = Number(fields[key].value).toFixed(fixed);
+		fields[key].value = Math.round((fields[key].value + Number.EPSILON) * dec) / dec;
 
 		// Grab values
 		values[key]      = parseFloat( fields[key].value );
